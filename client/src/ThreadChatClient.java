cff6bb68453d808ab707ce6f0d485e6e711fcd6d
Test <test@test.com>
2020-11-08 14:10:25 +0100

    Windows 10 notification

--- client/src/ThreadChatClient.java
+++ client/src/ThreadChatClient.java
@@ -65,19 +65,11 @@ public class ThreadChatClient implements Runnable
 	
 	private void SendWindows10Notification(String message)
 	{
-		//Obtain only one instance of the SystemTray object
-	    SystemTray tray = SystemTray.getSystemTray();
-
-	    // If you want to create an icon in the system tray to preview
+	    SystemTray tray = SystemTray.getSystemTray(); // ottenere system tray
 	    Image image = Toolkit.getDefaultToolkit().createImage("./logo.png");
-	    //Alternative (if the icon is on the classpath):
-	    //Image image = Toolkit.getDefaultToolkit().createImage(getClass().getResource("icon.png"));
-
 	    TrayIcon trayIcon = new TrayIcon(image, "Beatiful chat");
-	    //Let the system resize the image if needed
 	    trayIcon.setImageAutoSize(true);
-	    //Set tooltip text for the tray icon
-	    trayIcon.setToolTip("System tray icon demo");
+	    trayIcon.setToolTip("Beatiful chat");
 	    try {
 			tray.add(trayIcon);
 		} catch (AWTException e) {
